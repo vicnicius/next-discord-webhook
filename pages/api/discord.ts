c8ee8d66f918f3e98c6d2204e35f7615d04bcd46
Vinicius Andrade <vini.andrade.dev@gmail.com>
2022-08-19 12:02:31 +0200

    Get request raw body
    Signed-off-by: Vinicius Andrade <vini.andrade.dev@gmail.com>

--- pages/api/discord.ts
+++ pages/api/discord.ts
@@ -1,31 +1,51 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import type {Readable} from "node:stream";
 import {verifyKey} from "discord-interactions";
 
 type Data = {
   type: number
 }
 
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
 const DISCORD_PUBLIC_KEY = process.env.DISCORD_PUBLIC_KEY;
 
-export default function handler(
+async function buffer(readable: Readable) {
+  const chunks = [];
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === 'string' ? Buffer.from(chunk) : chunk);
+  }
+  return Buffer.concat(chunks);
+}
+
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const signature = req.headers["x-signature-ed25519"];
-  const timestamp = req.headers["x-signature-timestamp"];
+  if (req.method === 'POST') {
+    const signature = req.headers["x-signature-ed25519"];
+    const timestamp = req.headers["x-signature-timestamp"];
 
-  if (signature === undefined
-    || Array.isArray(signature)
-    || timestamp === undefined
-    || Array.isArray(timestamp)
-    || !DISCORD_PUBLIC_KEY
-    || !verifyKey(req.body, signature, timestamp, DISCORD_PUBLIC_KEY)
-  ) {
-    return res.status(401).end('Invalid request signature');
-  }
+    const buf = await buffer(req);
+    const rawBody = buf.toString('utf8');
 
-  if (req.body.type === 1) {
-    return res.status(200).json({ type: 1 })
+    if (signature === undefined
+      || Array.isArray(signature)
+      || timestamp === undefined
+      || Array.isArray(timestamp)
+      || !DISCORD_PUBLIC_KEY
+      || !verifyKey(rawBody, signature, timestamp, DISCORD_PUBLIC_KEY)
+    ) {
+      return res.status(401).end('Invalid request signature');
+    }
+
+    if (req.body.type === 1) {
+      return res.status(200).json({ type: 1 })
+    }
   }
 }
